51fbb576700d97825acd5b2eed0d85b2d4a34b4e
TOIsme <tuancong1462gmail.com>
2024-08-15 20:00:58 +0700

    Layout
    add SwiftGen

---
+++ CK/CK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CK
+//
+//  Created by MacBook Pro on 15/08/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
